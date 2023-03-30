77a0643f4beb59a286f902a92c544aca0d78298e
Mihir Shah <mihirs@cs.utexas.edu>
2023-03-29 19:23:52 -0500

    liburing integration

--- sharkdb/sharkdb/src/db_impl.cc
+++ sharkdb/sharkdb/src/db_impl.cc
@@ -4,11 +4,14 @@
 
 #include <cstdlib>
 
+#include <liburing.h>
+
 sharkdb_p sharkdb_init() {
     return new db_t();
 }
 
 void sharkdb_read(sharkdb_p db, const char* k, char* v_fill) {
+    db_t* p_db = (db_t*) db;
 }
 
 void sharkdb_update(sharkdb_p db, const char* k, const char* v) {
@@ -24,5 +27,5 @@ void sharkdb_scan(sharkdb_p db, const char* k, size_t len, char* buf) {
 }
 
 void sharkdb_free(sharkdb_p db) {
-    delete db;
+    delete ((db_t*) db);
 }
