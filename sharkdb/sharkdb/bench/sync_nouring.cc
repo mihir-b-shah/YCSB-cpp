60720f5664c45c1cf3099b9540a120038bc5ec1f
Mihir Shah <mihirs@cs.utexas.edu>
2023-04-03 17:49:04 -0500

    benchs

---
+++ sharkdb/sharkdb/bench/sync_nouring.cc
@@ -0,0 +1,17 @@
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <cassert>
+
+char buf[4096];
+
+int main() {
+	int fd = open("wal", O_CREAT | O_APPEND | O_WRONLY | O_SYNC, S_IWUSR);
+	assert(fd >= 0);
+	assert(unlink("wal") == 0);
+
+	for (size_t i = 0; i<10000; ++i) {
+		assert(write(fd, &buf[0], 4096) == 4096);
+	}
+	return 0;
+}
--- sharkdb/sharkdb/bench/sync_uring.cc
+++ sharkdb/sharkdb/bench/sync_uring.cc
@@ -1,17 +1,27 @@
 
 #include <unistd.h>
 #include <fcntl.h>
-#include <assert.h>
+#include <cassert>
+#include <liburing.h>
 
 char buf[4096];
+
+static constexpr size_t N_SQ_ENTRIES = 128;
 
 int main() {
 	int fd = open("wal", O_CREAT | O_APPEND | O_WRONLY | O_SYNC, S_IWUSR);
 	assert(fd >= 0);
 	assert(unlink("wal") == 0);
 
+    struct io_uring ring;
+    assert(io_uring_queue_init(N_SQ_ENTRIES, &ring, 0) == 0);
+
+    
+
 	for (size_t i = 0; i<1000; ++i) {
 		assert(write(fd, &buf[0], 4096) == 4096);
 	}
+
+    assert(io_uring_queue_exit(&ring) == 0);
 	return 0;
 }
