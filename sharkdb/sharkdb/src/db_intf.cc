36cb058aa76d6640c7a2b8ae6a621ea93d2d07c2
Mihir Shah <mihirs@cs.utexas.edu>
2023-04-20 07:54:56 -0500

    changes

--- sharkdb/sharkdb/src/db_intf.cc
+++ sharkdb/sharkdb/src/db_intf.cc
@@ -97,7 +97,10 @@ sharkdb_cqev sharkdb_read_async(sharkdb_t* db, const char* k, char* fill_v) {
 		memcpy(fill_v, v_found, SHARKDB_VAL_BYTES);
         //  lclk=0 suffices here, reads should become visible as soon as appear in queue.
         cq->emplace(part, 0, cqev, true);
+
+        #if defined(MEASURE)
         get_stats()->t_read_io_sync_ns_.push_back(get_ts_nsecs() - t_sync_before);
+        #endif
 	} else {
         /*  Scan bloom filters in order, until I find a match.
             Issue a read for the range of blocks the fence ptr tells me about.
